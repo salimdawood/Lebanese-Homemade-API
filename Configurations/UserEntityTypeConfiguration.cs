dfc88619651ce6bf636e2da0290bdb1787882b8c
salimdawood <salimdawoodcareer@gmail.com>
2022-05-12 18:27:07 +0300

    basic-change-commit

---
+++ Configurations/UserEntityTypeConfiguration.cs
@@ -0,0 +1,20 @@
+﻿using LebaneseHomemadeLibrary;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace LebaneseHomemade.Configurations
+{
+    public class UserEntityTypeConfiguration : IEntityTypeConfiguration<UserModel>
+    {
+        public void Configure(EntityTypeBuilder<UserModel> builder)
+        {
+            builder.Property(prop => prop.Name).IsRequired();
+            builder.Property(prop => prop.Email).IsRequired();
+            builder.Property(prop => prop.Password).IsRequired();
+        }
+    }
+}
--- Data/AppDbContext.cs
+++ Data/AppDbContext.cs
@@ -1,4 +1,5 @@
-﻿using LebaneseHomemadeLibrary;
+﻿using LebaneseHomemade.Configurations;
+using LebaneseHomemadeLibrary;
 using Microsoft.EntityFrameworkCore;
 using System;
 using System.Collections.Generic;
@@ -9,15 +10,20 @@ namespace LebaneseHomemade.Data
 {
     public class AppDbContext : DbContext
     {
-        public AppDbContext(DbContextOptions<AppDbContext> options) : base(options)
-        {
+        public AppDbContext(DbContextOptions<AppDbContext> options) : base(options){}
 
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            new UserEntityTypeConfiguration().Configure(modelBuilder.Entity<UserModel>());
         }
+
         public DbSet<UserModel> Users { get; set; }
+        /*
         public DbSet<CardModel> Cards { get; set; }
         public DbSet<MenuModel> Menus { get; set; }
         public DbSet<TypeModel> Types { get; set; }
         public DbSet<PhotoModel> Photos { get; set; }
         public DbSet<ItemModel> Items { get; set; }
+        */
     }
 }
